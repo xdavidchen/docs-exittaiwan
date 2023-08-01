bce13220b6cdeb77bb46781f117307960e08dc76
xdavidchen <xdavidchen@tutamail.com>
2023-08-02 01:25:17 +0800

    added beam analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+import Head from '@docusaurus/Head';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -39,3 +40,13 @@ export default function Home() {
     </Layout>
   );
 }
+
+const MySEO = () => (
+  <Head>
+    <meta property="og:description" content="這裡有你出國需要的教學 / 攻略 / 工具" />
+    <meta charSet="utf-8" />
+    <title>知識庫 💡</title>
+    <link rel="canonical" href="https://docs.xdavidchen.com" />
+    <script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="b347b00d-9393-4bb3-8cb9-7daaf1a18057" async></script>
+  </Head>
+);
