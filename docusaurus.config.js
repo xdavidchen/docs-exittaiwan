6abbcbd27ebf1d22b91d39757598c278a70a44fc
xdavidchen <xdavidchen@tutamail.com>
2023-08-25 13:34:33 +0800

    updated logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,10 +81,10 @@ const config = {
       },
       navbar: {
         title: 'David 知識庫',
-        /*logo: {
+        logo: {
           alt: 'David Chen Logo',
           src: 'img/logo.svg',
-        },*/
+        },
         items: [
           {
             type: 'docSidebar',
